6dde0d0dda7aef5249026581b968097b7cb6a9d5
Naveen Mandadhi <nmandadhi@solstice.com>
2020-02-20 14:27:06 -0600

    Version 1.0-beta1

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-address=$(kubectl get svc geode -n geode -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+address=$(kubectl get svc geode -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 if [[ $address == "" ]]; then
-  address=$(kubectl get svc geode -n geode -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+  address=$(kubectl get svc geode -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
   if [[ $address == "" ]]; then
     echo "External IP is not yet available, try in sometime ..."
     exit 1
   fi
 fi
-port=$(kubectl get svc geode -n geode -o jsonpath='{.spec.ports[?(@.name == "http")].port}')
+port=$(kubectl get svc geode -o jsonpath='{.spec.ports[?(@.name == "http")].port}')
 
 gfsh -e "start pulse --url=http://$address:$port/pulse"
 gfsh -e "connect --use-http=true --url=http://$address:$port/geode-mgmt/v1"
